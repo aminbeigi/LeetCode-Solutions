73d9568b5f8768b63219ebe4b295511266388aa8
Amin Ghasembeigi <39206588+aminbeigi@users.noreply.github.com>
2020-06-14 14:58:05 +1000

    Create LinkedListCycle.java

---
+++ Java/LinkedListCycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode (int val) {
+ *         this.val = val;
+ *     }
+ * }
+ */
+ public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null){
+            return false;
+        }
+        // Floyd's cycle-detection algortihm
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+ 
