4162caf5eafd807f31a4c294839fb4f2babb010c
Amin Ghasembeigi <39206588+aminbeigi@users.noreply.github.com>
2020-06-14 14:36:04 +1000

    Create binary-linked-list-to-integer.java

---
+++ Java/binary-linked-list-to-integer.java
@@ -0,0 +1,29 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode (int val) {
+        this.val = val;
+    }
+}
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        ListNode current = head;
+        int count = 0;
+        int decOutput = 0;
+
+        while (current != null) {
+            current = current.next;
+            ++count;
+        }
+
+        // reset value of current node
+        current = head;
+        while (count > 0) {
+            decOutput = decOutput + (current.val * (int)(Math.pow(2, count-1)));
+            current = current.next;
+            --count;
+        }
+        return decOutput;
+    }
+}
+    
