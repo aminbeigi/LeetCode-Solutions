a3eb16e0d74588f8f5543de987e79ac5fc056d28
Amin Ghasembeigi <me@aminbeigi.com>
2020-07-06 23:54:47 +1000

    Fixing the indentation

--- Java/Easy/single-number/Solution.java
+++ Java/Easy/single-number/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     // bitwise operator XOR solution
     // time complexity: O(n)
     // space complexity: O(1)
-public int singleNumber(int[] nums) {
+	public int singleNumber(int[] nums) {
         int n = 0;
         for (int i : nums) {
             n = n ^ i;
