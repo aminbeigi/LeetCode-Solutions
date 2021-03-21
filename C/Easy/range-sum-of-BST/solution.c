c08dbb6abb220bc39843a2f6c572c7994efbe357
aminbeigi <me@aminbeigi.com>
2021-03-21 13:42:25 +1100

    Add better solution

---
+++ C/Easy/range-sum-of-BST/solution.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+/*
+ * recursive solution
+ * time complexity: O(n)
+ * space complexity: O(n)
+*/
+int rangeSumBST(struct TreeNode* root, int low, int high){
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (root->val > high) {
+        return rangeSumBST(root->left, low, high);
+    }
+
+    if (root->val < low) {
+        return rangeSumBST(root->right, low, high);
+    }
+
+    return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high) + root->val;
+}
+
+/*
+ * recursive brute-force solution
+ * time complexity: O(n)
+ * space complexity: O(n)
+*/
+int rangeSumBST2(struct TreeNode* root, int low, int high){
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (root->val >= low && root->val <= high) {
+        return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high) + root->val;
+
+    } else {
+        return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
+}
