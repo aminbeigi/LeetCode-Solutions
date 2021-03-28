6a7194db9ee5411bca14614d3904b8f265a4cf1a
Amin Ghasembeigi <me@aminbeigi.com>
2021-03-28 14:26:46 +1100

    Add else if block

--- C/Easy/range-sum-of-BST/solution.c
+++ C/Easy/range-sum-of-BST/solution.c
@@ -16,12 +16,12 @@ int rangeSumBST(struct TreeNode* root, int low, int high){
         return 0;
     }
 
-    if (root->val > high) {
-        return rangeSumBST(root->left, low, high);
-    }
 
     if (root->val < low) {
         return rangeSumBST(root->right, low, high);
+    
+    } else if (root->val > high) {
+        return rangeSumBST(root->left, low, high);
     }
 
     return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high) + root->val;
