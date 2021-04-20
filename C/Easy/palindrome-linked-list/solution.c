df864d4fa62e54f2bbcab888c2093dc5fe1f804e
aminbeigi <me@aminbeigi.com>
2021-04-20 16:27:52 +1000

    Add solution

---
+++ C/Easy/palindrome-linked-list/solution.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* reverseList(struct ListNode* head);
+
+/*
+ * reverse solution
+ * time complexity: O(n)
+ * space complexity: O(1)
+*/
+bool isPalindrome(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        // empty or single element list is a palindrome
+        return true;
+    }
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    slow->next = reverseList(slow->next); // reverse second half of list
+    slow = slow->next;
+    while (slow != NULL){
+        if (head->val != slow->val) {
+            return false;
+        }
+        head=head->next;
+        slow=slow->next;
+    }
+    return true;
+}
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* tmp;
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+
+    while (curr != NULL) {
+        tmp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = tmp;
+    }
+    return prev;
+}
