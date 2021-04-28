c57bca56542f6b9c0605a938f4fe8c22de0b5c1a
aminbeigi <me@aminbeigi.com>
2021-04-28 20:43:03 +1000

    Add time complexity

--- C/Easy/same-tree/solution.c
+++ C/Easy/same-tree/solution.c
@@ -7,6 +7,11 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+/*
+ * recursive solution
+ * time complexity: O(n)
+ * space complexity: O(n)
+*/
 bool isSameTree(struct TreeNode *p, struct TreeNode *q) {
     if (p == NULL || q == NULL) {
         return (p == q);
