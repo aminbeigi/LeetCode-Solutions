7aaaa239e883f241038632e2e08c18511742efa7
Amin Ghasembeigi <me@aminbeigi.com>
2021-05-21 11:14:29 +1000

    Create solution.py

---
+++ Python/Easy/majority-element/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for num in nums:
+            if num not in d:
+                d[num] = 1
+            else:
+                d[num] += 1
+            if d[num] >= len(nums)/2:
+                return num
